3d9db69e8038ac8f7030ed12ee75785a1cdac74d
Sidi Beqiraga <sib@dataone.de>
2023-05-12 12:31:13 +0200

    added customElement

--- myTable.js
+++ myTable.js
@@ -1,36 +1,37 @@
 import { html,LitElement} from 'https://cdn.jsdelivr.net/gh/lit/dist@2/all/lit-all.min.js';
-
-export class MyElement extends LitElement{
-    static properties = {
+// define the component
+export class HelloWorld extends LitElement {
+  
+  static properties = {
+    who: {type: String},
+  };
+  
+  // return a promise for contract changes.
+  static getMetaConfig() {
+    return {
+      controlName: 'Hello World',
+      fallbackDisableSubmit: false,
+      version: '1.2',
+      properties: {
         who: {
-            type: String
+          type: 'string',
+          title: 'Who',
+          description: 'Who to say hello to'
         }
-    };
-
-    static getMetaConfig() {
-        return {
-          controlName: 'Hello World',
-          fallbackDisableSubmit: false,
-          version: '1.2',
-          properties: {
-            who: {
-              type: 'string',
-              title: 'Who',
-              description: 'Who to say hello to'
-            }
-          }
-        };
       }
-    
-    constructor(){
-        super();
-    }
-    
-    render(){
-        return html`<p> Hello <p/>`;
-    }
+    };
+  }
+  
+  constructor() {
+    super();
+    this.who = 'World';
+  }
 
+  render() {
+    return html`<p>Hello ${this.who}<p/>`;
+  }
 }
 
-const customElement = "my-table-v1";
-customElements.define(customElement, MyElement);
+// registering the web component
+const elementName = 'hello-world';
+customElements.define(elementName, HelloWorld);
