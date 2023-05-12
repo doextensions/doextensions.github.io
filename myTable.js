85a0bee876f68c15114dea31ec84d42926ff20c6
Sidi Beqiraga <sib@dataone.de>
2023-05-12 12:15:07 +0200

    initial commit

---
+++ myTable.js
@@ -0,0 +1,32 @@
+import { html, LitElement, css } from 'lit';
+
+export class MyElement extends LitElement{
+    static properties = {
+        who: {
+            type: String
+        }
+    }
+
+    static getMetaConfig() {
+        return {
+          controlName: 'Hello World',
+          fallbackDisableSubmit: false,
+          version: '1.2',
+          properties: {
+            who: {
+              type: 'string',
+              title: 'Who',
+              description: 'Who to say hello to'
+            }
+          }
+        };
+      }
+    
+    constructor(){
+        super();
+    }
+    
+    render(){
+        return html`<p> Hello <p/>`;
+    }
+}
