1d802e45f3099c3f3907467db4bb3d261fc68d5a
Michael Hoff <mho@dataone.de>
2023-06-02 11:03:01 +0200

    conditional

--- myTable.js
+++ myTable.js
@@ -633,7 +633,7 @@ const girdJsStyles = i$3`
   td.gridjs-td:last-child {
     border-right: none;
   }
-  td.gridjs-td:last-child:empty tr {
+  .gridjs-tr td.gridjs-td:last-child:empty {
     background-color: #cc0000;
   }
   td.gridjs-message {
